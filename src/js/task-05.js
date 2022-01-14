9efc11e46a5dc8e69ac5881e7c4e5bfe7f4ebfe2
valeriavoronova <valeriavoronova1990@gmail.com>
2022-01-14 22:00:18 +0200

    Update task-05.js

---
+++ src/js/task-05.js
@@ -0,0 +1,7 @@
+// Напиши скрипт который, при наборе текста в инпуте input#name-input 
+// (событие input), 
+// подставляет его текущее значение в span#name-output. 
+// Если инпут пустой, в спане должна отображаться строка "Anonymous".
+
+// <input type="text" id="name-input" placeholder="Please enter your name" />
+// <h1>Hello, <span id="name-output">Anonymous</span>!</h1>
