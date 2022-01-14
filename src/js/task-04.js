fa10dad77ada9a34e5f395cf819c4724c58d4a92
valeriavoronova <valeriavoronova1990@gmail.com>
2022-01-14 21:19:09 +0200

    Update task-04.js

---
+++ src/js/task-04.js
@@ -0,0 +1,25 @@
+// Счетчик состоит из спана и кнопок, которые, при клике, должны увеличивать и уменьшать его значение на единицу.
+
+// <div id="counter">
+//   <button type="button" data-action="decrement">-1</button>
+//   <span id="value">0</span>
+//   <button type="button" data-action="increment">+1</button>
+// </div>
+// Создай переменную counterValue в которой будет храниться текущее значение счетчика и инициализируй её значением 0.
+// Добавь слушатели кликов на кнопки, внутри которых увеличивай или уменьшай значение счтетчика.
+// Обновляй интерфейс новым значением переменной counterValue
+
+let counterValue = 0;
+const incrementRef = document.querySelector('button[data-action="increment"]');
+const decrementRef = document.querySelector('button[data-action="decrement"]');
+//console.log(incrementRef)
+const value = document.querySelector("#value");
+value.textContent = counterValue;
+
+const clickI = () => { counterValue += 1; }
+
+const clickD = () => { counterValue -= 1; }
+
+incrementRef.addEventListener("click", clickI);
+decrementRef.addEventListener("click", clickD);
+console.log(counterValue);
