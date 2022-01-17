4275be518621e3496fd459c36fbf7efeb5bc7be0
valeriavoronova <valeriavoronova1990@gmail.com>
2022-01-17 02:42:15 +0200

    Update task-09.js

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -4,3 +4,14 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const bodyRef = document.querySelector("body");
+const buttonRef = document.querySelector(".change-color");
+const descrRef = document.querySelector(".color");
+
+buttonRef.addEventListener("click", setRandomHexColor);
+
+function setRandomHexColor() {
+  bodyRef.style.backgroundColor = getRandomHexColor();
+  descrRef.textContent = bodyRef.style.backgroundColor;
+}
