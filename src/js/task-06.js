3e0b895bccc0eb0b7b5dbc566d0c4e04d5a922ad
valeriavoronova <valeriavoronova1990@gmail.com>
2022-01-17 03:11:46 +0200

    213

--- src/js/task-06.js
+++ src/js/task-06.js
@@ -16,19 +16,25 @@ function unFocusHandler(event) {
     //console.log("текущая длина ввода", event.currentTarget.value.length);
     // console.log("заданный параметр", inputRef.dataset.length);
     // console.log("они равны?", event.currentTarget.value.length == inputRef.dataset.length);
-    if (event.currentTarget.value.length == inputRef.dataset.length) {
+    function removeClasses (){
         event.currentTarget.classList.remove("invalid");
+        event.currentTarget.classList.remove("valid");
+    }
+
+    if (event.currentTarget.value.length == inputRef.dataset.length) {
+        removeClasses ();
         event.currentTarget.classList.add("valid");    
     }
 
     else if(event.currentTarget.value.length === 0){
-        event.currentTarget.classList.remove("invalid");
-        event.currentTarget.classList.remove("valid")
+        // event.currentTarget.classList.remove("invalid");
+        // event.currentTarget.classList.remove("valid");
+        removeClasses ();
         
     }
 
     else {
-        event.currentTarget.classList.remove("valid");
+        removeClasses ();
         event.currentTarget.classList.add("invalid");
     }
         
--- src/js/task-10.js
+++ src/js/task-10.js
@@ -1,3 +1,22 @@
+// Напиши скрипт создания и очистки коллекции элементов. Пользователь вводит количество элементов в input и нажимает кнопку Создать, после чего рендерится коллекция. При нажатии на кнопку Очистить, коллекция элементов очищается.
+
+// <div id="controls">
+//   <input type="number" min="1" max="100" step="1" />
+//   <button type="button" data-create>Create</button>
+//   <button type="button" data-destroy>Destroy</button>
+// </div>
+
+// <div id="boxes"></div>
+// Создай функцию createBoxes(amount), которая принимает один параметр - число. Функция создает столько <div>, сколько указано в amount и добавляет их в div#boxes.
+
+// Размеры самого первого <div> - 30px на 30px.
+// Каждый элемент после первого, должен быть шире и выше предыдущего на 10px.
+// Все элементы должены иметь случайный цвет фона в формате HEX. Используй готовую функцию getRandomHexColor для получения цвета.
+// function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+// }
+// Создай функцию destroyBoxes(), которая очищает содержимое div#boxes, тем самым удаляя все созданные элементы.
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
