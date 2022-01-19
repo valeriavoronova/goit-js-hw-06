a7156a478a673a06c79195a72da6b3dbf34b099f
Valeria Voronova <66879713+valeriavoronova@users.noreply.github.com>
2022-01-19 10:19:43 +0200

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -8,11 +8,16 @@ console.log("Number of categories:", itemsRef.length);
 
 const titleRef = document.querySelectorAll("h2");
 
-    for(const text of titleRef){
-            console.log("\nCategory:",text.textContent);
-            console.log("Elements:",text.nextElementSibling.children.length);
+    // for(const text of titleRef){
+    //         console.log("\nCategory:",text.textContent);
+    //         console.log("Elements:",text.nextElementSibling.children.length);
 
-        }
+    //     }
+
+titleRef.forEach((currentValue) => { 
+    console.log("\nCategory:",currentValue.textContent);
+    console.log("Elements:",currentValue.nextElementSibling.children.length);
+})
 
 
 
